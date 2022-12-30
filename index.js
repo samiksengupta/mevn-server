dc5e28c8bc474f4ef230beeaf22c963f5faafa97
samiksengupta <samiksengupta@hotmail.com>
2022-12-30 10:44:55 +0530

    added routers

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { HOST, PORT, ENV } = require('./config/server');
-const masterRouter = require('./routers');
+const mainRouter = require('./routers');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(masterRouter);
+app.use(mainRouter);
 
 const server = app.listen(PORT, () => {
     process.stdout.write(`Server started at ${HOST}:${PORT} (${ENV})\n`);
